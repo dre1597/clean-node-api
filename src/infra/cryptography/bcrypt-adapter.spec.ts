15d7f9a3fe01e3f6a2f19bac1035d44daa5b3a82
Andre Pereira <andrepereira97@yahoo.com>
2021-10-08 16:02:54 -0300

    test: ensure BcryptAdapter throws an exception if bcrypt throws an exception

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -27,4 +27,12 @@ describe('Bcrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+
+  test('Should throws an exception if bcrypt throws an exception', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
