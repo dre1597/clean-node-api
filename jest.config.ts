aa5edc429b1007ec1b7047fb9e6049faae880791
Andre Pereira <andrepereira97@yahoo.com>
2021-10-11 12:38:27 -0300

    refactor: remove main layer from test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   transform: {
