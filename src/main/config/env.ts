6e9f8b6a168d80e2443d072287ebaff020b53dc3
Andre Pereira <andrepereira97@yahoo.com>
2021-10-14 12:05:08 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   port: process.env.PORT || 5050,
-  mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api'
+  mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5H3'
 }
