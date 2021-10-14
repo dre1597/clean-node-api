fd6d2fb7156e2951d6b25dcde66ca43379acdfb4
Andre Pereira <andrepereira97@yahoo.com>
2021-10-14 15:06:34 -0300

    refactor: change mongo url variable on env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   port: process.env.PORT || 5050,
-  mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H3'
 }
