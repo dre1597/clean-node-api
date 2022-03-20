6f56b2aa25e90857e3cdd3c3eb16d3863c6baa40
André Pereira <andrepereira97@yahoo.com>
2022-03-20 18:07:28 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,15 +1,19 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbiddenError } from '@/presentation/helpers/http/http-helper'
+import { forbiddenError, internalServerError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-    if (!survey) {
-      return forbiddenError(new InvalidParamError('surveyId'))
+    try {
+      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      if (!survey) {
+        return forbiddenError(new InvalidParamError('surveyId'))
+      }
+      return Promise.resolve(null)
+    } catch (error) {
+      return internalServerError(error)
     }
-    return Promise.resolve(null)
   }
 }
